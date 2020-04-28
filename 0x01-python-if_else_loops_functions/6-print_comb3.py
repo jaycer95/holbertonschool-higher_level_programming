e226f44d7bac6b3ea40b4e00d9b5b993928fc111
jaycer95 <jacer.dabbabi95@gmail.com>
2020-04-28 16:12:17 +0000

    booored

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,100):
-    if i < 10:
-        if (i % 10) > (i / 10):
-            print("{:02d}, ".format(i), end="")
-    elif i < 99:
-        if (i % 10) > (i / 10):
-            print("{:d}, ".format(i), end="")
-        if i == 89:
-            print("{:d}".format(i))
+for i in range(0,9):
+    for j in range(1,10):
+        if j > i:
+            if (i == 8 and j == 9):
+                print("{:d}{:d}".format(i, j))
+            else:
+                print("{:d}{:d}".format(i, j), end=", ")
