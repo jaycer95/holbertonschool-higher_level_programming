f4776845c9ee591e055572b805c82de15c119c00
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-05 14:58:34 +0000

    Max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    else:
+        x = 0
+        for i in my_list:
+            if i >= x:
+                x = i
+        return x
