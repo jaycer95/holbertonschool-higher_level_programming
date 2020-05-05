1ea8f0ccfc56379a3fac18a6dc721df975d591d4
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-05 14:09:10 +0000

    Try

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentense is None:
+        tup = (0, None)
+        return tup
+    else:
+        x = len(sentence)
+        y = sentence[0]
+        tup = (x, y)
+        return tup
