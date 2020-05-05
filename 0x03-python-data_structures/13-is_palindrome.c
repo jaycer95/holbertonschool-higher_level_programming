c55a827c3c40b67e32c06bc513d14d5ec62ed628
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-05 21:21:21 +0000

    malloc

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -19,7 +19,7 @@ int is_palindrome(listint_t **head)
 		len++;
 	}
 	adv = *head;
-	s = malloc(sizeof(len));
+	s = malloc(sizeof(int) * len);
 	while (adv)
 	{
 		s[i] = adv->n;
@@ -31,8 +31,10 @@ int is_palindrome(listint_t **head)
 	{
 		if (s[i] != s[len - i - 1])
 		{
+			free(s);
 			return (0);
 		}
 	}
+	free(s);
 	return (1);
 }
