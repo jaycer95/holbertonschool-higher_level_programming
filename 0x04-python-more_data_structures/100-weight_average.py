9e6d3fd37b9e80aedc67e899bc7b305df97e391e
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-07 17:53:35 +0000

    correct

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,9 @@
 def weight_average(my_list=[]):
     if my_list == []:
         return 0
-    mul = lambda x, y : x * y
     a = 0
     b = 0
     for i in my_list:
-        a += mul(i[0],i[1])
+        a += i[0] * i[1]
         b += i[1]
     return a / b
