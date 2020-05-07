20eecebabc7b97dcdea6bfa1d3c329309675041c
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-07 18:02:25 +0000

    list comprehension

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,4 @@
 def weight_average(my_list=[]):
     if my_list == []:
         return 0
-    a = 0
-    b = 0
-    for i in my_list:
-        a += i[0] * i[1]
-        b += i[1]
-    return a / b
+    return sum(list((x * y) for x,y in my_list)) / sum(i[1] for i in my_list)
