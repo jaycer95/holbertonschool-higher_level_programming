6ce81af845eb8f6c57fbad214c2c95306f365d91
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-19 04:33:59 +0000

    no check

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" defining a class"""
+class Square:
+    """ class Square that defines a square """
+    def __init__(self, size):
+        """ private attribute """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creating a Square class """
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ Sets the initial size of the new square object """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
