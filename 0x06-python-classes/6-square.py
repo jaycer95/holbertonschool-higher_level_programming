90a321f5788b4ce43d98a2490b04deb00c266e88
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-19 20:11:28 +0000

    euuubh

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,23 @@ class Square:
     """ Square class """
     def __init__(self, size=0, position=(0, 0)):
         """ Sets the initial size of the new square object """
-        self.size = size
-        self.position = position
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+        if (type(position) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(position[0], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(position[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0 or len(position) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
 
     @property
     def size(self):
