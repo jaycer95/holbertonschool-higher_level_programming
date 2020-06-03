b36a8dd86a83104df9df12a4c0d7f1ef20c0487a
jaycer95 <jacer.dabbabi95@gmail.com>
2020-06-03 22:27:00 +0000

    13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" create a student class"""
+
+
+class Student:
+    """ defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) is False:
+            return self.__dict__
+
+        dict = {}
+        for strings in attrs:
+            if type(strings) is not str:
+                return self.__dict__
+            if strings in self.__dict__:
+                dict[strings] = self.__dict__[strings]
+        return dict
+
+    def reload_from_json(self, json):
+        for att in json.keys():
+            self.__dict__[att] = json[att]
