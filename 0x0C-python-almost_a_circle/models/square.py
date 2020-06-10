bee5cc7700fe15567e7814544456e898fd118e01
jaycer95 <jacer.dabbabi95@gmail.com>
2020-06-10 09:06:07 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,3 +34,26 @@ class Square(Rectangle):
 
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update """
+
+        for i, arg in enumerate(args):
+            if i == 0:
+                self.id = arg
+            if i == 1:
+                self.size = arg
+            if i == 2:
+                self.x = arg
+            if i == 3:
+                self.y = arg
+
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "size":
+                self.size = value
+            if key == "x":
+                self.x = value
+            if key == "y":
+                self.y = value
