745ce7195c4a0d6599352177182a0460c90aacf6
jaycer95 <jacer.dabbabi95@gmail.com>
2020-07-16 17:59:42 +0100

    output check

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 --  List all privileges of the MySQL users user_0d_1 and user_0d_2 
+--  This can be done with the "SHOW GRANTS FOR command"
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
