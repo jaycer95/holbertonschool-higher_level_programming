038aa11b2829c56f843e6ac7227376481feb0816
jaycer95 <jacer.dabbabi95@gmail.com>
2020-08-09 17:20:59 +0000

    Task 5 > 7

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" DAtabase """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities WHERE state_id = (SELECT
+                id FROM states WHERE name='%s')""" % argv[4])
+    query_rows = cur.fetchall()
+    print(", ".join(row[0] for row in query_rows))
+    cur.close()
+    db.close()
