a95334dd74dc163dabfdaebaaab7a51a9fbc4520
jaycer95 <jacer.dabbabi95@gmail.com>
2020-09-13 05:27:05 -0700

    advanced

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #send a request to a URL passed as an argument, and display only the status code of the response.
-curl -o -I -L -s -w "%{http_code}" $1
+curl -sL -o /dev/null -I -w "%{http_code}" $1
